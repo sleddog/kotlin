5772bfb305f250a61f3a6b2e1a6b1cb2690f9ad4
Devin Gray <dgray@figure.com>
2018-05-13 14:28:37 -0600

    solve #3

--- projecteuler/src/main.kt
+++ projecteuler/src/main.kt
@@ -3,6 +3,7 @@ package projecteuler
 fun main(args : Array<String>) {
     //var answer = problem1(1000)
     //var answer = problem2()
-    var answer = problem3(13195)
+    var answer = problem3(600851475143)
     print(answer)
+
 }
